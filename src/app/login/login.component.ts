3b7773b7a8c47fb46df278d6f2d6a12f49fac4c6
kubrakyazi <kna.innova@gmail.com>
2021-05-11 00:01:10 +0300

    routingBefore

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,3 @@
-import { ThrowStmt } from '@angular/compiler';
-
 import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormControl,Validators,FormBuilder } from "@angular/forms";
 
@@ -8,27 +6,55 @@ import { FormGroup,FormControl,Validators,FormBuilder } from "@angular/forms";
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
   loginForm:FormGroup;
 
   constructor(private formBuilder:FormBuilder) {
-      
+    
     this.loginForm=this.formBuilder.group({
+      firstName:["",Validators.required],
+      lastName:["",Validators.required],
       email:["",Validators.required],
+      city:["",Validators.required],
       password:["",Validators.required]
     })
 
    }
 
   ngOnInit(): void {
-   
+   this.checkLoginInfo()
   }
+  
 
   login(){
     if(this.loginForm.valid){
       console.log(this.loginForm.value)
     }
+    localStorage.setItem("LoginInfo",JSON.stringify(this.loginForm.value))
+    if(JSON.parse(<string>localStorage.getItem("LoginInfo")).email==this.loginForm.value.email)
+    {
+      console.log("başarılı oldu")
+    }
+    else{
+      console.log("başarısız")
+    }
+   
+    
+  }
+
+  checkLoginInfo(){
+    if(localStorage.getItem("LoginInfo"))
+    {
+      //this.loginForm<FormGroup>=JSON.parse(localStorage.getItem("LoginInfo",this.loginForm.value))
+      console.log(localStorage.getItem("LoginInfo"));
+     
+    }
+    else{
+     // alert("")
+    }
+    
   }
   
 }
